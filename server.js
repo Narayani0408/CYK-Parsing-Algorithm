bc37bf9bd8aba82ad4544d43a8ebb9fdd9673611
Narayani <narayaniwadhai@gmail.com>
2026-09-10 20:38:27 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -19,7 +19,12 @@ const server = http.createServer((req, res) => {
       res.writeHead(404, { 'Content-Type': 'text/plain' });
       res.end('404 Not Found');
     } else {
-      res.writeHead(200, { 'Content-Type': contentType });
+      res.writeHead(200, {
+        'Content-Type': contentType,
+        'Cache-Control': 'no-cache, no-store, must-revalidate',
+        'Pragma': 'no-cache',
+        'Expires': '0'
+      });
       res.end(content);
     }
   });
